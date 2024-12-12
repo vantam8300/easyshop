aa3218b97fbee67aa66d7be5c9d9d7c86ca7d751
Van Tam Chau <vantam8300@gmail.com>
2024-12-12 11:37:54 -0800

    add method to shoppingcart interface

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,13 @@
 package org.yearup.data;
 
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
 
 public interface ShoppingCartDao
 {
-    ShoppingCart getByUserId(int userId);
     // add additional method signatures here
+    ShoppingCart getByUserId(int userId);
+    ShoppingCart addItem(int userId, Product product);
+    ShoppingCart updateItem(int userId, int productId, Product product);
+    ShoppingCart deleteItem(int userId, int productId);
 }
